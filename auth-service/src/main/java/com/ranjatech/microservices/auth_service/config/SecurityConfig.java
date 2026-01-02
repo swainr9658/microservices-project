10ecdf10203283d5e24673879b8c5c9f419b3f2e
ranjan <swainr9658@gmail.com>
2026-01-02 22:36:13 +0530

    test123hjh127wrr127

--- auth-service/src/main/java/com/ranjatech/microservices/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/ranjatech/microservices/auth_service/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.ranjatech.microservices.auth_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +17,11 @@ public class SecurityConfig {
 				.requestMatchers("/actuator/**").permitAll().anyRequest().authenticated());
 
 		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
